e905e2d79f5d700cbf53011850b48f77c5d534ab
David Maust <david@dmaust.net>
2015-03-26 13:09:11 -0700

    Adding API Key

--- client.py
+++ client.py
@@ -4,9 +4,14 @@ import json
 import requests
 from pprint import pprint
 
-post_data = {
-    "text":"I enjoy writing to APIs with my favorite programming",
-    "apikey": "demo"
-}
-response = requests.post("http://demo.predictkey.com/predict", json=post_data)
-pprint(response.json())
+def get_prediction(text):
+    post_data = {
+        "text":text,
+        "apikey": "demo"
+    }
+    response = requests.post("http://demo.predictkey.com/predict", json=post_data)
+    return response.json()
+
+if __name__=="__main__":
+    pred = get_prediction("I enjoy writing to APIs with my favorite programming")
+    pprint(pred)
