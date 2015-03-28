b9cfc04e6dafe5d69c1a489c9a6e8a05a469172d
dmaust <david@dmaust.net>
2015-03-28 14:13:27 -0700

    Adding partial word support.

--- client.py
+++ client.py
@@ -4,16 +4,23 @@ import json
 import requests
 from pprint import pprint
 
-def get_prediction(text):
+def get_prediction(text, partial_words=False):
     post_data = {
         "text":text,
-        "apikey": "demo"
+        "apikey": "demo",
+        "partial": partial_words
     }
     response = requests.post("http://api.predictkey.com/predict", json=post_data)
     return response.json()
 
-if __name__=="__main__":
-    pred = get_prediction("I enjoy writing to APIs with my favorite programming")
+def show_prediction_debug(text, partial_words=False):
+    pred = get_prediction(text, partial_words=partial_words)
+    print "Predictions for:", text
     print "Top Prediction:", pred['results'][0]['word']
     print "Full output"
     pprint(pred)
+
+if __name__=="__main__":
+    show_prediction_debug("I enjoy writing to APIs with my favorite programming")
+    show_prediction_debug("Have a good d", partial_words=True)
+    show_prediction_debug("Have a good n", partial_words=True)
